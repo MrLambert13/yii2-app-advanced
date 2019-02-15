8ea2137252f0f2c8da44d6358d967555965947cf
Mr.L@mbert_13 <Lam_13@mail.ru>
2019-02-15 16:45:56 +0300

    Lesson 1, Ex 1, 2 done

--- console/migrations/m130524_201442_init.php
+++ console/migrations/m130524_201442_init.php
@@ -20,7 +20,7 @@ class m130524_201442_init extends Migration
             'password_reset_token' => $this->string()->unique(),
             'email' => $this->string()->notNull()->unique(),
             'access_token' => $this->string(),
-            'avatar' > $this->string(),
+            'avatar' => $this->string(),
 
             'status' => $this->smallInteger()->notNull()->defaultValue(10),
             'created_at' => $this->integer()->notNull(),
