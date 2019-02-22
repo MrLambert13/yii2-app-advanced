faa015c86e2c8b681db4f39bccb1bbf4818a7043
Mr.L@mbert_13 <Lam_13@mail.ru>
2019-02-22 16:02:37 +0300

    add ratchet

--- backend/config/main.php
+++ backend/config/main.php
@@ -11,7 +11,8 @@ return [
     'basePath' => dirname(__DIR__),
     'controllerNamespace' => 'backend\controllers',
     'bootstrap' => ['log'],
-    'modules' => [],
+    'modules' => [
+    ],
     'components' => [
         'request' => [
             'csrfParam' => '_csrf-backend',
